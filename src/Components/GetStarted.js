d6d21bd29da7ffb4385177be5b96e30db3f9b40f
Mitchell Crane <mitchellcrane@Mitchells-MacBook-Pro.local>
2022-02-07 11:52:54 -0700

    update blink link

--- src/Components/GetStarted.js
+++ src/Components/GetStarted.js
@@ -16,7 +16,7 @@ function GetStarted() {
           title="Loan Application"
           description="Create your account to apply with
           First Class Home Mortgage."
-          url="https://www.blink.mortgage/app/signup/p/firstclasshomemortgagellc/karifitzgerald"
+          url="https://www.blink.mortgage/app/signup/p/FirstClassHomeMortgage/karifitzgerald"
           iconName="applicationIcon"
           btnText="Start Application"
         />
